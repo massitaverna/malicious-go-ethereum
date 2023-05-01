811176ae4245c0bd65d4caa012bba19e4a2a1c2b
massi <mataverna@ethz.ch>
2023-05-01 11:57:34 +0200

    p2p: remove prints of meaningless errors

--- p2p/peer.go
+++ p2p/peer.go
@@ -279,7 +279,7 @@ loop:
 		//log.Error("FOR")
 		select {
 		case err = <-writeErr:
-			log.Error("Write")
+			//log.Error("Write")
 			// A write finished. Allow the next write to start if
 			// there was no error.
 			if err != nil {
@@ -288,7 +288,7 @@ loop:
 			}
 			writeStart <- struct{}{}
 		case err = <-readErr:
-			log.Error("Read")
+			//log.Error("Read")
 			if r, ok := err.(DiscReason); ok {
 				log.Error("Requested by remote")
 				remoteRequested = true
@@ -309,11 +309,11 @@ loop:
 			}
 			break loop
 		case err = <-p.protoErr:
-			log.Error("Proto")
+			//log.Error("Proto")
 			reason = discReasonForError(err)
 			break loop
 		case err = <-p.disc:
-			log.Error("Disc")
+			//log.Error("Disc")
 			reason = discReasonForError(err)
 			break loop
 		}
